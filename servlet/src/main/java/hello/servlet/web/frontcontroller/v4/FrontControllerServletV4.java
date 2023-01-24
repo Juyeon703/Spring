a359b0087a75b58e6078e7c52d3e7bcb157e3911
Juyeon703 <sh6244@naver.com>
2023-01-25 07:45:48 +0900

    단순하고 실용적인 컨트롤러

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,69 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.rmi.MarshalledObject;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 앞서 만든 v3 컨트롤러는 서블릿 종속성을 제거하고 뷰 경로의 중복을 제거하는 등, 잘 설계된 컨트롤러이다.
+ * 그런데 실제 컨트톨러 인터페이스를 구현하는 개발자 입장에서 보면, 항상 ModelView 객체를 생성하고 반환해야 하는 부분이 조금은 번거롭다.
+ * 좋은 프레임워크는 아키텍처도 중요하지만, 그와 더불어 실제 개발하는 개발자가 단순하고 편리하게 사용할 수 있어야 한다. 소위 실용성이 있어야 한다.
+ * 이번에는 v3를 조금 변경해서 실제 구현하는 개발자들이 매우 편리하게 개발할 수 있는 v4 버전을 개발해보자.
+ */
+
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+  private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+  public FrontControllerServletV4() {
+    controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+    controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+    controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    System.out.println("FrontControllerServletV4.service");
+
+    String requestURI = request.getRequestURI();
+
+    ControllerV4 controller = controllerMap.get(requestURI); // MemberListControllerV1()
+    if (controller == null) {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404
+      return;
+    }
+
+    Map<String, String> paramMap = createParamMap(request);
+    Map<String, Object> model = new HashMap<>();
+    String viewName = controller.process(paramMap, model);
+
+    MyView view = viewResolver(viewName);
+
+    view.render(model, request, response);
+  }
+
+  private static MyView viewResolver(String viewName) {
+    return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+  }
+
+  private static Map<String, String> createParamMap(HttpServletRequest request) {
+    //paramMap
+    Map<String, String> paramMap = new HashMap<>();
+    request.getParameterNames().asIterator()
+      .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+    return paramMap;
+  }
+}
