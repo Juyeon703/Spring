52b91c2e01207f9a96a5cb79a2ea1aec9d84c223
Juyeon703 <sh6244@naver.com>
2023-01-25 06:28:11 +0900

    .

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/FrontControllerServletV1.java
@@ -13,6 +13,11 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 서블릿과 비슷한 모양의 컨트롤러 인터페이스를 도입한다.
+ * 각 컨트롤러들은 이 인터페이스를 구현하면 된다.
+ * 프론트 컨트롤러는 이 인터페이스를 호출해서 구현과 관계없이 로직의 일관성을 가져갈 수 있다.
+ */
 @WebServlet(name = "frontControllerServletV1", urlPatterns = "/front-controller/v1/*")
 public class FrontControllerServletV1 extends HttpServlet {
 
